0e995fbadee1ff107c2e6013fee9d707cf74e95a
Shaydullin Ilfat <sweden.fan@yandex.ru>
2026-05-29 22:47:42 +0300

    created Expense table

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, nullable=False) # user_id telegram
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    amount: Mapped[int] = mapped_column(Integer, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
