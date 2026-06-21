fef58e5704399b6d28967478f8e7bff921cb01f3
Shaydullin Ilfat <sweden.fan@yandex.ru>
2026-06-21 15:49:41 +0300

    added new rout bot_expenses

--- app/main.py
+++ app/main.py
@@ -7,11 +7,13 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from app.expenses.router import router as router_expenses
+from app.expenses.bot_router import router as bot_expenses_router
 
 
 app = FastAPI()
 
 app.include_router(router_expenses)
+app.include_router(bot_expenses_router)
 
 
 if __name__ == "__main__":
